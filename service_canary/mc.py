a012177ea2dc71d51f6dc196299d7cdc56443879
Daniel Brandt <daniel@loadimpact.com>
2017-03-30 10:42:05 +0200

    memcached test.

--- service_canary/mc.py
+++ service_canary/mc.py
@@ -1,2 +1,20 @@
+import sys
+import socket
+try:
+    from pymemcache.client.base import Client
+except ImportError:
+    print("memcached test requested, but pymecache not installed. "
+          "Try 'pip install pymemcache' and try again.")
+    sys.exit(1)
+
 def mc_check(config):
-    return False
+    host = config.get("host", "localhost")
+    port = int(config.get("port", 11211))
+    try:
+        mc = Client((host, port))
+        mc.stats()
+    except:
+        return False
+    else:
+        return True
+
