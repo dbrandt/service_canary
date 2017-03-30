ed857dcdf60b4ace626ff92ff96f2015eea6445a
Daniel Brandt <daniel@loadimpact.com>
2017-03-30 11:21:52 +0200

    HTTP API test.

--- service_canary/api.py
+++ service_canary/api.py
@@ -1,2 +1,26 @@
+import requests
+
+
 def api_check(config):
-    return False
+    url = config.get("url")  # Existance of url is checked in args.
+    ep = config.get("endpoint")
+    token = config.get("token")
+
+    headers = {}
+    if token is not None:
+        headers["Authorization"] = "Token {}".format(token)
+    if not url.endswith("/"):
+        url += "/"
+    if ep is not None:
+        if ep.startswith("/"):
+            url += ep[1:]
+        else:
+            url += ep
+
+    try:
+        r = requests.get(url, headers=headers)
+    except:
+        return False
+    else:
+        return True
+
